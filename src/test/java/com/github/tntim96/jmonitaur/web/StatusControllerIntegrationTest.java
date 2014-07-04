a424723e5dfc0cb8a5bb01bcc5df21e95884aaaf
tntim96 <tntim96@gmail.com>
2014-07-05 08:51:53 +1000

    Add MockMvc test

---
+++ src/test/java/com/github/tntim96/jmonitaur/web/StatusControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.github.tntim96.jmonitaur.web;
+
+import com.github.tntim96.jmonitaur.WebUiApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = WebUiApplication.class)
+public class StatusControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testHome() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>JMonitaur - Status</title>")));
+    }
+
+}
